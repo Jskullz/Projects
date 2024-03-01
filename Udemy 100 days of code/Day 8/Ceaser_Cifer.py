1db4ad3546787e6d82ea489750cf840c8a97d992
Jskullz <juliannascully@gmail.com>
2024-03-01 15:47:00 -0500

    Add new files

---
+++ Udemy 100 days of code/Day 8/Ceaser_Cifer.py
@@ -0,0 +1,45 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+program = True
+while program is True:
+    if input("Do you want to decode or encode a message? (yes/no): ") == "yes":
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+
+        def ceasar(text,shift,direction):
+            plain_text = ""
+            if direction == "decode":
+                shift *= -1
+            for letter in text:
+                if letter in alphabet:
+                    position = alphabet.index(letter)
+                    new_position = (position + shift) % 26
+                    plain_text += alphabet[new_position]
+                else:
+                    plain_text += letter
+            
+            print(f"The {direction}d text is {plain_text}")
+        ceasar(text,shift,direction)
+    else:
+        print("Goodbye")
+        input("Press enter to exit")
+        program = False
+    
---
+++ Udemy 100 days of code/Day 9/Dictionary.py
@@ -0,0 +1,13 @@
+programming_dictionary = {
+"Bug": "An error in a program that prevents the program from running as expected.", 
+"Function": "A piece of code that you can easily call over and over again.",
+}
+
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+programming_dictionary["Bug"] = "A moth in your computer."
+
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
+    
